faaebc1d756f4224e433dc4e97a9d05ec4bf7e89
Eclipse_K <godo8522@naver.com>
2022-11-22 21:34:15 +0900

    Add Map randering

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,6 +1,33 @@
+function formatData(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
+function ReviewListItem({ item }) {
+  return (
+    <div>
+      <img src={item.imgUrl} alt={item.title} />
+      <div>
+        <h1>{item.title}</h1>
+        <p>{item.rating}</p>
+        <p>{item.formatData(item.createdAt)}</p>
+        <p>{item.content}</p>
+      </div>
+    </div>
+  );
+}
 function ReviewList({ item }) {
-  console.log(item);
-  return <ul></ul>;
+  return (
+    <ul>
+      {item.map((item) => {
+        return (
+          <li>
+            <ReviewListItem item={item} />
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default ReviewList;
