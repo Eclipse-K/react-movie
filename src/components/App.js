886db8deb6c3c9af3ff7ffa8648bbd29fbdda24f
Eclipse_K <godo8522@naver.com>
2022-11-24 21:28:41 +0900

    Add api.js & handleLoadClick button

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { getReviews } from "../api";
 import ReviewList from "./ReviewList";
 
 function App() {
@@ -15,6 +16,11 @@ function App() {
     setItems(nextItems);
   };
 
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews();
+    setItems(reviews);
+  };
+
   return (
     <div>
       <div>
@@ -22,6 +28,7 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList item={sortedItems} onDelete={handleDeleteClick} />
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
