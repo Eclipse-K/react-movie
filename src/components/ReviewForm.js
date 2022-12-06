d872212947e049cbc59921625b2eb9e91fc692b1
Eclipse_K <godo8522@naver.com>
2022-12-06 21:50:14 +0900

    Add ReviewForm.js in handleTitleChange

---
+++ src/components/ReviewForm.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function ReviewForm() {
+  const [title, setTitle] = useState("");
+}
+
+const handleTitleChange = (e) => {
+  setTitle(e.target.value);
+};
+
+return (
+  <form>
+    <input value={title} onChange={handleTitleChange} />
+  </form>
+);
