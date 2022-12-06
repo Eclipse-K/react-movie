a5abfbbfe50615b573b2f9565b92e8075541b980
Eclipse_K <godo8522@naver.com>
2022-12-06 21:57:39 +0900

    Add handleRatingChange & handleContentChange

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -2,14 +2,29 @@ import { useState } from "react";
 
 function ReviewForm() {
   const [title, setTitle] = useState("");
+  const [rating, setRating] = useState(0);
+  const [content, setContent] = useState("");
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleRatingChange = (e) => {
+    const nextRating = Number(e.target.value);
+    setRating(nextRating);
+  };
+
+  const handleContentChange = (e) => {
+    setContent(e.target.value);
+  };
+
+  return (
+    <form>
+      <input value={title} onChange={handleTitleChange} />
+      <input type="number" value={rating} onChange={handleRatingChange} />
+      <textarea value={content} onChange={handleContentChange} />
+    </form>
+  );
 }
 
-const handleTitleChange = (e) => {
-  setTitle(e.target.value);
-};
-
-return (
-  <form>
-    <input value={title} onChange={handleTitleChange} />
-  </form>
-);
+export default ReviewForm;
