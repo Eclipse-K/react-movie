2a20c933980805a8383dd37f09028a911e19949e
Eclipse_K <godo8522@naver.com>
2022-12-08 22:29:51 +0900

    handleChange state binding

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -2,21 +2,31 @@ import { useState } from "react";
 import "./ReviewForm.css";
 
 function ReviewForm() {
-  const [title, setTitle] = useState("");
+  const [value, setValue] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+  });
+  /*const [title, setTitle] = useState("");
   const [rating, setRating] = useState(0);
   const [content, setContent] = useState("");
-
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
   };
-
   const handleRatingChange = (e) => {
     const nextRating = Number(e.target.value);
     setRating(nextRating);
   };
-
   const handleContentChange = (e) => {
     setContent(e.target.value);
+  }; */
+
+  const handleChange = (e) => {
+    const [name, value] = e.target;
+    setValue((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
   };
 
   const handleSubmit = (e) => {
@@ -26,9 +36,9 @@ function ReviewForm() {
 
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
-      <input value={title} onChange={handleTitleChange} />
-      <input type="number" value={rating} onChange={handleRatingChange} />
-      <textarea value={content} onChange={handleContentChange} />
+      <input value={title} onChange={handleChange} />
+      <input type="number" value={rating} onChange={handleChange} />
+      <textarea value={content} onChange={handleChange} />
       <button type="submit">확인</button>
     </form>
   );
