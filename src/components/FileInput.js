e2453890996e0a9ac8d5b2709a29f83e5fd82907
Eclipse_K <godo8522@naver.com>
2022-12-13 16:43:27 +0900

    Add useRef

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,10 +1,14 @@
+import { useRef } from "react";
+
 function FileInput({ name, value, onChange }) {
+  const inputRef = useRef();
+
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
 
-  return <input type="file" onChange={handleChange} />;
+  return <input type="file" onChange={handleChange} ref={inputRef} />;
 } //FileInput은 반드시 비제어 컴포넌트로
 
 export default FileInput;
