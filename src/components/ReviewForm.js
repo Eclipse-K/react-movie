bdae842775dfa8bb5f4e2b9b808a302a67c85db8
Eclipse_K <godo8522@naver.com>
2022-12-25 22:01:49 +0900

    Add Reflect Response Data

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -11,7 +11,7 @@ const INITIAL_VALUES = {
   imgFile: null,
 };
 
-function ReviewForm() {
+function ReviewForm({ onSubmitSuccess }) {
   const [values, setValues] = useState(INITIAL_VALUES);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [submittingError, setSubmittingError] = useState(null);
@@ -48,17 +48,22 @@ function ReviewForm() {
     formData.append("rating, values.rating");
     formData.append("content, values.content");
     formData.append("imgFile, values.imgFile");
+
+    let result;
     try {
       setSubmittingError(null);
       setIsSubmitting(true);
-      await createReviews(formData);
+      result = await createReviews(formData);
     } catch (error) {
       setSubmittingError(error);
       return;
     } finally {
       setIsSubmitting(false);
     }
+
+    const { review } = result;
     setValues(INITIAL_VALUES);
+    onSubmitSuccess(review);
   };
 
   return (
